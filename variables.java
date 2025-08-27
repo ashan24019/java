961cb0b1f6db8f3bb91bb96a1e406f57b5142fa2
Ashan <ashanvimodh241@gmail.com>
2025-08-27 20:47:29 +0530

    java

--- variables.java
+++ variables.java
@@ -1,6 +1,9 @@
+import java.util.Date;
+
 public class variables {
     public static void main(String[] args) {
         int age = 20;
-        System.out.println(age);
+        Date now = new Date();
+        System.out.println(now);
     }
 }
